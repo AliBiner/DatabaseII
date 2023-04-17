9f0360788e72b72f8e4a489ea59c9c8b60ec4f89
Ali Biner <alibiner.github@gmail.com>
2023-04-17 05:16:45 +0300

    Update Hafta4.sql
    Math function and string function learned

---
+++ Hafta4.sql
@@ -0,0 +1,25 @@
+--use mart7
+
+--select upper(pAd),upper(pSoyad) from personel
+
+--select upper('hakan Yüksel') as AdSoyad
+
+--select lower(pAd) from personel
+
+--select len(pAd) from personel
+
+--select ltrim('        hakan')
+--select rtrim('hakan      ')
+--select trim('    hakan     ')
+
+--select left('Veritabaný II',4)
+--select right('Veritabaný II',4)
+
+--select substring('Veritabaný II',5,1)
+
+--select substring('Veritabaný II',5,3)
+
+--select 'Sayýn ' + upper(left(pAd,1)) + '. ' + upper(pSoyad) from personel
+--select replace('www.isparta.edu.tr','edu','gov')
+
+--select replicate('ISUBU',5)
